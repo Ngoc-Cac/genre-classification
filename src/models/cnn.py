61ede88635a46abf1d03cda90d25af25b54b7c62
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-11-01 15:57:18 +0700

    updated cnn model
    - Changed the convolution to 2
    d convolution on both the frequency and time axis\n-Added some more linear layers at the end to increase the netowrk's depth

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -8,6 +8,7 @@ class CNNSpec(nn.Module):
         num_labels: int,
         in_channels: int,
         inner_channels: int = (128, 128, 256, 256, 512),
+        num_linear_layers: int = 3,
         kernel_size: int = 3,
         downsampling_rate: int = 2,
         activation_fn: str | nn.Module = 'relu'
@@ -23,28 +24,35 @@ class CNNSpec(nn.Module):
         self.model = nn.Sequential()
         for layer, out_channels in enumerate(inner_channels):
             conv_block = nn.Sequential(
-                nn.Conv1d(
+                nn.Conv2d(
                     in_channels, out_channels,
                     kernel_size, padding=1
                 ),
-                nn.BatchNorm1d(out_channels), activation_fn,
-                nn.MaxPool1d(
+                nn.BatchNorm2d(out_channels), activation_fn,
+                nn.AvgPool2d(
                     kernel_size, downsampling_rate, 1
                 )
             )
-            self.model.add_module(
-                f'conv_block_{layer}',
-                conv_block
-            )
+            self.model.add_module(f'conv_block_{layer}', conv_block)
             in_channels = out_channels
 
         self.model.add_module(
-            'output_logits',
+            'global_avg_pool',
             nn.Sequential(
-                nn.AdaptiveAvgPool1d(1), nn.Flatten(),
-                nn.Linear(out_channels, num_labels)
+                nn.AdaptiveAvgPool2d(1), nn.Flatten(),
             )
         )
+
+        for layer in range(num_linear_layers):
+            self.model.add_module(
+                f'linear_{layer}',
+                nn.Sequential(
+                    nn.Linear(out_channels, out_channels),
+                    activation_fn,
+                )
+            )
+
+        self.model.add_module('output_logits', nn.Linear(out_channels, num_labels))
 
     def forward(self, spectrograms):
         return self.model(spectrograms)
