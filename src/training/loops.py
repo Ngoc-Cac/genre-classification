c9016a997689daef6c7ca0a0cca5a27d2a8fb94a
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-11-23 07:41:13 +0700

    updated type hints for loops.py
    - updated type hint for callables
    - added return type

--- src/training/loops.py
+++ src/training/loops.py
@@ -6,14 +6,14 @@ from typing import Callable, Literal
 def train_loop(
     model: torch.nn.Module,
     train_loader: torch.utils.data.DataLoader,
-    loss_fn: Callable,
+    loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
     optimizer: torch.optim.Optimizer,
     gradient_scaler: torch.GradScaler,
     device: Literal['cpu', 'cuda'],
     *,
     mixed_precision: bool = False,
-    callback_fn: Callable | None = None
-):
+    callback_fn: Callable[[int, float]] | None = None
+) -> tuple[float, float]:
     if callback_fn is None:
         callback_fn = lambda *args: None
     train_loss, train_acc = 0, 0
@@ -42,11 +42,11 @@ def train_loop(
 def eval_loop(
     model: torch.nn.Module,
     test_loader: torch.utils.data.DataLoader,
-    loss_fn: Callable,
+    loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
     device: Literal['cpu', 'cuda'],
     *,
     mixed_precision: bool = False
-):
+) -> tuple[float, float]:
     test_loss, test_acc = 0, 0
     for data, labels in test_loader:
         labels = labels.to(device)
