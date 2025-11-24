0db56ca9e263da15548daec65bb4c9a95b1a31bf
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-11-24 08:11:20 +0700

    added random clipping to clip_signal
    added a random cropping to clip_signal utility function, this will crop the signa starting at a random location such that the cropped signal is duration-second-long.

--- src/data_utils/processing_utils.py
+++ src/data_utils/processing_utils.py
@@ -1,14 +1,26 @@
+import random
+
+from numpy.typing import ArrayLike
+
+
 def clip_signal(
-    signal,
+    signal: ArrayLike,
     sampling_rate: int,
-    start: int | float,
-    duration: int | float
-):
-    start_idx = int(start * sampling_rate)
-    end_idx = (start + duration) * sampling_rate
-    end_idx = int(end_idx) + bool(end_idx % 1)  # quirky round up
-    if end_idx >= len(signal):
-        end_idx = len(signal) - 1
+    duration: int | float,
+    start: int | float | None = None,
+) -> ArrayLike:
+    if not len(signal):
+        return signal[:0]
+
+    num_samples = duration * sampling_rate
+    # quirky round up because n % 1 > 0 iff n has a decimal part
+    num_samples = min(int(num_samples) + bool(num_samples % 1), len(signal))
+
+    start_idx = (
+        random.randint(0, len(signal) - num_samples)
+        if start is None else int(start * sampling_rate)
+    )
+    end_idx = min(start_idx + num_samples - 1, len(signal) - 1)
 
     return (
         signal[start_idx:(end_idx + 1)]
