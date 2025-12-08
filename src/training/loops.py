b0891d900f4ff7ef445f772e3646bd28b5e4b403
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-12-08 19:58:17 +0700

    renamed arguments

--- src/training/loops.py
+++ src/training/loops.py
@@ -5,7 +5,7 @@ from typing import Callable, Literal, Any
 
 def train_loop(
     model: torch.nn.Module,
-    train_loader: torch.utils.data.DataLoader,
+    dataloader: torch.utils.data.DataLoader,
     loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
     optimizer: torch.optim.Optimizer,
     gradient_scaler: torch.GradScaler,
@@ -17,7 +17,7 @@ def train_loop(
     if callback_fn is None:
         callback_fn = lambda *args: None
     train_loss, train_acc = 0, 0
-    for step, (data, labels) in enumerate(train_loader):
+    for step, (data, labels) in enumerate(dataloader):
         labels = labels.to(device)
         with torch.autocast(
             device_type=device,
@@ -42,14 +42,14 @@ def train_loop(
 @torch.no_grad()
 def eval_loop(
     model: torch.nn.Module,
-    test_loader: torch.utils.data.DataLoader,
+    dataloader: torch.utils.data.DataLoader,
     loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
     device: Literal['cpu', 'cuda'],
     *,
     mixed_precision: bool = False
 ) -> tuple[float, float]:
     test_loss, test_acc = 0, 0
-    for data, labels in test_loader:
+    for data, labels in dataloader:
         labels = labels.to(device)
         with torch.autocast(
             device_type=device,
